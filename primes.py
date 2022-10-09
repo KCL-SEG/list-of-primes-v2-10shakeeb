7516eeac5573deb160362af732d50bed697bc94e
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-10-09 13:31:05 +0100

    done

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import itertools
 
 def primes(number_of_primes):
-    list = []
+    D = {}
+    q = 2
+
+    while True:
+        if number_of_primes <= 0:
+            raise ValueError
+        elif q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+            q += 1
+
+    list = list(itertools.islice(gen_primes(), number_of_primes))
     return list
