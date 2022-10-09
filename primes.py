44bca7e8872d346d10c30d6c6ba6727acfca1135
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-10-09 14:18:28 +0100

    fixed

--- primes.py
+++ primes.py
@@ -3,13 +3,23 @@
 import itertools
 
 def primes(number_of_primes):
-    list =[]
-    if number_of_primes > 0:
-        chkthis = 2
-        while len(primes) < N:
-            ptest    = [chkthis for i in primes if chkthis%i == 0]
-            list  += [] if ptest else [chkthis]
-            chkthis += 1
-    else:
+    list = []
+    number = 2
+
+    if number_of_primes <= 0:
         raise ValueError
+
+    while(len(list) < number_of_primes):
+        is_prime = True
+
+        for prime in list:
+            if(number % prime == 0):
+                is_prime = False
+                break
+
+        if(is_prime):
+            list.append(number)
+
+        number += 1
+
     return list
