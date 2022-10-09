22b0f84644b74111e0438a8bf527a8aa419ee57a
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-10-09 13:35:36 +0100

    fixed

--- primes.py
+++ primes.py
@@ -3,20 +3,13 @@
 import itertools
 
 def primes(number_of_primes):
-    D = {}
-    q = 2
-
-    while True:
-        if number_of_primes <= 0:
-            raise ValueError
-        elif q not in D:
-            yield q
-            D[q * q] = [q]
-        else:
-            for p in D[q]:
-                D.setdefault(p + q, []).append(p)
-            del D[q]
-            q += 1
-
-    list = list(itertools.islice(gen_primes(), number_of_primes))
+    list =[]
+    if number_of_primes > 0:
+        chkthis = 2
+        while len(primes) < N:
+            ptest    = [chkthis for i in primes if chkthis%i == 0]
+            list  += [] if ptest else [chkthis]
+            chkthis += 1
+    else:
+        raise ValueError
     return list
